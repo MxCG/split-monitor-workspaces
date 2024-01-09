f171d5c830fc721ba1ac811acdaba135634588ba
MaxCG <mcgoeritz@gmail.com>
2024-01-09 00:34:34 -0300

    Implement special and handle multiple monitors connecting

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,24 @@ const std::string k_workspaceCount = "plugin:split-monitor-workspaces:count";
 const std::string k_keepFocused = "plugin:split-monitor-workspaces:keep_focused";
 const CColor s_pluginColor = {0x61 / 255.0f, 0xAF / 255.0f, 0xEF / 255.0f, 1.0f};
 
+static HOOK_CALLBACK_FN* e_monitorAddedHandle = nullptr;
+static HOOK_CALLBACK_FN* e_monitorRemovedHandle = nullptr;
+
 const uint32_t COUNT_PER_MONITOR = 10;
 const uint32_t SPECIAL_BASE = 1e5;
 const uint32_t WORKSPACE_BASE = 1;
 const uint32_t SPECIAL_COUNT = 5;
+const uint32_t MAX_MONITORS = 5;
 
 void notify(std::string msg) {
 	 HyprlandAPI::addNotification(PHANDLE, msg, s_pluginColor, 5000);
+}
+
+std::string token(std::istream& is) {
+		std::string t;
+		if (!std::getline(is, t, ' '))
+			throw std::invalid_argument("Could not get token");
+		return t;
 }
 
 /*
@@ -33,8 +44,24 @@ typedef int workspaceId;
 struct Workspace {
 	bool isSpecial;
 	uint64_t id;
-
 };
+
+struct Window {
+	CWindow* handle;
+
+	Window(std::istream& is) {
+		handle = nullptr;
+		auto handleStr = token(is);
+		if (handleStr == "c") {
+			handle = g_pCompositor->windowFromCursor();
+		} else {
+			handleStr = handleStr.substr(handleStr.size() - 8);
+			uint32_t handleNum = std::stoul(handleStr, nullptr, 16);
+			handle = g_pCompositor->getWindowFromHandle(handleNum);
+		}
+	}
+};
+
 
 class Monitor {
 private:
@@ -44,10 +71,12 @@ private:
 
 	}
 
+public:
 	auto ID() {
 		return ptr->ID;
 	}
 
+private:
 	workspaceId toWorkspaceId(Workspace workspace) {
 		if (workspace.isSpecial) {
 			if (workspace.id >= SPECIAL_COUNT)
@@ -56,20 +85,21 @@ private:
 			return SPECIAL_BASE + workspace.id * SPECIAL_COUNT + ID();
 		} else {
 			if (workspace.id >= COUNT_PER_MONITOR)
-				throw std::invalid_argument("Invalid workspace id");
+				throw std::invalid_argument("Invalid workspace id " + std::to_string(workspace.id));
 
 			return WORKSPACE_BASE + ID() * COUNT_PER_MONITOR + workspace.id;
 		}
 	}
 
-	bool owns(workspaceId wId) {
-		if (wId >= SPECIAL_BASE) {
-			if (wId - SPECIAL_BASE < ID()) return false;
-			return (wId - SPECIAL_BASE - ID()) % SPECIAL_COUNT == 0;
-		} else {
-			return (wId - WORKSPACE_BASE) / COUNT_PER_MONITOR == ID();
+	CWorkspace* toWorkspacePtr(Workspace w) {
+		auto w_id = toWorkspaceId(w);
+		CWorkspace* w_ptr = g_pCompositor->getWorkspaceByID(w_id);
+		if (!w_ptr) {
+			w_ptr = g_pCompositor->createNewWorkspace(w_id, ID(), (w.isSpecial ? "special " : "") + std::to_string(w.id + 1));
 		}
+		return w_ptr;
 	}
+
 
 	Workspace toWorkspace(workspaceId workspaceId) {
 		if (workspaceId >= SPECIAL_BASE) {
@@ -82,6 +112,15 @@ private:
 				.isSpecial = false,
 				.id = workspaceId - WORKSPACE_BASE - ID() * COUNT_PER_MONITOR,
 			};
+		}
+	}
+
+	bool owns(workspaceId wId) {
+		if (wId >= SPECIAL_BASE) {
+			if (wId - SPECIAL_BASE < ID()) return false;
+			return (wId - SPECIAL_BASE - ID()) % SPECIAL_COUNT == 0;
+		} else {
+			return (wId - WORKSPACE_BASE) / COUNT_PER_MONITOR == ID();
 		}
 	}
 
@@ -99,6 +138,15 @@ public:
 		return Monitor(ptr);
 	}
 
+	static std::vector<Monitor> all() {
+		std::vector<Monitor> monitors;
+		for (const auto& ptr: g_pCompositor->m_vMonitors) {
+			Monitor m(ptr.get());
+			monitors.push_back(m);
+		}
+		return monitors;
+	}
+
 	Workspace getActiveWorkspace() {
 		workspaceId active = ptr->activeWorkspace;
 		return toWorkspace(active);
@@ -106,9 +154,11 @@ public:
 
 	std::vector<Workspace> getWorkspaces() {
 		std::vector<Workspace> workspaces;
-		for (const std::unique_ptr<CWorkspace>& w_ptr: g_pCompositor->m_vWorkspaces) {
+		for (const auto& w_ptr: g_pCompositor->m_vWorkspaces) {
 			if (owns(w_ptr->m_iID)) {
-				workspaces.push_back(toWorkspace(w_ptr->m_iID));
+				auto w = toWorkspace(w_ptr->m_iID);
+				if (!w.isSpecial)
+					workspaces.push_back(toWorkspace(w_ptr->m_iID));
 			}
 		}
 		std::sort(workspaces.begin(), workspaces.end(), [](auto a, auto b) { return a.id < b.id; });;
@@ -116,14 +166,13 @@ public:
 	}
 
 	void setActiveWorkspace(Workspace w) {
-		auto w_id = toWorkspaceId(w);
-
-		auto w_ptr = g_pCompositor->getWorkspaceByID(w_id);
-		if (!w_ptr) {
-			w_ptr = g_pCompositor->createNewWorkspace(w_id, ID(), std::to_string(w.id + 1));
-		}
-
+		auto w_ptr = toWorkspacePtr(w);
 		ptr->changeWorkspace(w_ptr);
+	}
+
+	void pickWindow(Workspace workspace, Window window) {
+		auto w_ptr = toWorkspacePtr(workspace);
+		g_pCompositor->moveWindowToWorkspaceSafe(window.handle, w_ptr);
 	}
 };
 
@@ -131,29 +180,31 @@ struct UWID {
 	Monitor monitor;
 	Workspace workspace;
 
-	// (monitor_id | c) (a workspace_id |s special_id|(e|r) delta_id)
+	// (c|a monitor_id) (a workspace_id |s special_id|(e|r) delta_id)
 	UWID(std::istream& UWIDs) {
-		auto token = [&UWIDs]() {
-			std::string t;
-			if (!std::getline(UWIDs, t, ' '))
-				throw std::invalid_argument("Could not parse UWID");
-			return t;
+		auto number = [&UWIDs]() {
+			return std::stoi(token(UWIDs));
 		};
-		auto number = [&token]() {
-			return std::stoi(token());
+		auto methodChar = [&UWIDs]() {
+			auto tk = token(UWIDs);
+			if (tk.size() > 1)
+				std::invalid_argument("Selection method has more than one char");
+			return tk[0];
 		};
 		auto deltaMod = [](int a, int b, int mod) {
 			return (a + (b % mod) + mod) % mod;
 		};
 
-		auto monitor_selector = token();
-		if (monitor_selector == "c") {
-			monitor = Monitor::current();
-		} else {
-			throw std::logic_error("Monitor selection uninplemented");
+		switch (methodChar()) {
+			case 'c':
+				monitor = Monitor::current();
+				break;
+			case 'a':
+				monitor = Monitor::fromId(number());
+				break;
 		}
 
-		auto workspace_selection_method = token();
+		auto workspace_selection_method = token(UWIDs);
 		if (workspace_selection_method.size() > 1)
 			throw std::invalid_argument("Invalid workspace selection method");
 
@@ -210,18 +261,71 @@ struct UWID {
 };
 
 // (window_id | c) UWID
-void moveWindowToWorkspace() {
-	throw std::logic_error("Not implemented");
+void moveWindowToWorkspace(std::string in) {
+	std::stringstream ins(in);
+	Window w(ins);
+	UWID uwid(ins);
+	uwid.monitor.pickWindow(uwid.workspace, w);
+}
+
+void toggleSpecialInternal(Workspace workspace) {
+	static uint64_t last_workspaces_id[MAX_MONITORS];
+
+	if (!workspace.isSpecial)
+		throw std::invalid_argument("toggleSpecial with a common workspace");
+
+	auto monitors = Monitor::all();
+
+	Workspace cur = Monitor::current().getActiveWorkspace();
+	if (cur.isSpecial && cur.id == workspace.id) {
+		for (Monitor& m : monitors) {
+			m.setActiveWorkspace({.isSpecial = false, .id = last_workspaces_id[m.ID()]});
+		}
+		return;
+	}
+
+	if (!cur.isSpecial) {
+		for (Monitor& m : monitors) {
+			auto w = m.getActiveWorkspace();
+			if (w.isSpecial == false) {
+				last_workspaces_id[m.ID()] = w.id;
+			}
+		}
+	}
+
+	for (Monitor& m : Monitor::all()) {
+		m.setActiveWorkspace(workspace);
+	}
+}
+
+void toggleSpecial(std::string in) {
+	uint64_t id = std::stoul(in) - 1;
+	toggleSpecialInternal({.isSpecial = true, .id = id});
 }
 
 // UWID
 void focusWorkspace(std::string in) {
+	auto cur = Monitor::current().getActiveWorkspace();
+	if (cur.isSpecial) {
+		toggleSpecialInternal(cur);
+		return;
+	}
 	UWID uwid(in);
 	if (uwid.workspace.isSpecial)
 		throw std::invalid_argument("Cannot focus a special workspace on a single monitor");
 	uwid.monitor.setActiveWorkspace(uwid.workspace);
 }
 
+void resetAllMonitors() {
+	Workspace w = {.isSpecial = false, .id = 0};
+	for (Monitor m : Monitor::all()) {
+		m.setActiveWorkspace(w);
+	}
+}
+
+void onMonitorAdd(void*, SCallbackInfo&, std::any) {
+	resetAllMonitors();
+}
 
 // Do NOT change this function.
 APICALL EXPORT std::string PLUGIN_API_VERSION()
@@ -233,10 +337,18 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
 {
 	PHANDLE = handle;
 
+	resetAllMonitors();
+
 	HyprlandAPI::addConfigValue(PHANDLE, k_workspaceCount, SConfigValue{.intValue = 10});
 	HyprlandAPI::addConfigValue(PHANDLE, k_keepFocused, SConfigValue{.intValue = 0});
 
 	HyprlandAPI::addDispatcher(PHANDLE, "change_workspace", focusWorkspace);
+	HyprlandAPI::addDispatcher(PHANDLE, "move_window_to_workspace", moveWindowToWorkspace);
+	HyprlandAPI::addDispatcher(PHANDLE, "toggle_special", toggleSpecial);
+
+	e_monitorAddedHandle = HyprlandAPI::registerCallbackDynamic(PHANDLE, "monitorAdded", onMonitorAdd);
+	// e_monitorRemovedHandle = HyprlandAPI::registerCallbackDynamic(PHANDLE, "monitorRemoved", refreshMapping);
+
 	return {"split-monitor-workspaces", "Split monitor workspace namespaces", "Duckonaut", "1.1.0"};
 }
 
